5e78566950b3c0196a4542fb7cf47e447841a413
Yashi Gupta <yashig406@gmail.coM>
2023-03-28 08:42:44 +0530

    adding

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('player.png')
 playerX = 370 
 playerY = 480 #lower value  inverse to height
-# playerX_change = 0
-# playerY_change = 0
+playerX_change = 0
+playerY_change = 0
 
 def player(x,y):
     screen.blit(playerImg,(x,y)) #blit means to draw
@@ -27,13 +27,25 @@ running = True
 while running:
      #rgb - red, green, blue (0 to 255)
     screen.fill((0,0,0))
-  
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             running = False 
-
-
     
+        # if key-stroke is pressed check whether it's right or left
+        if event.type == pygame.KEYDOWN:
+            print("KEYSTROKE PRESSED(anykey)")
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+                # print("LEFT ARROW")
+            if event.key == pygame.K_RIGHT:
+                # print("RIGHT ARROW")
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+                # print("KEYSTROKE HAS RELEASED")
 
+    playerX += playerX_change
     player(playerX,playerY)
     pygame.display.update() #MANDATORY
