2d165ec31470d7b08eb07c07e9f339dbac3778a9
Yashi Gupta <yashig406@gmail.coM>
2023-03-29 22:30:48 +0530

    adding

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ pygame.init() #MANDATORY
 #to creating the screen
 screen = pygame.display.set_mode((800, 600)) #800 = width and 600 = height
 
+#Background
+bg = pygame.image.load('background.png')
+
 #title and icon 
 pygame.display.set_caption("Space Invanders")
 icon = pygame.image.load('ufo.png')
@@ -22,9 +25,20 @@ playerY_change = 0
 #enemy 
 enemyImg = pygame.image.load('enemy.png')
 enemyX = random.randint(0,800)
-enemyY = (50,150) #lower value  inverse to height
-enemyX_change = 0
-enemyY_change = 0
+enemyY = random.randint(50,150) #lower value  inverse to height
+enemyX_change = 4
+enemyY_change = 40
+
+#bullet
+#ready - you can't see the bullet on screen
+#fire - bullet is currently moving
+
+bulletImg = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480 #lower value  inverse to height
+bulletX_change = 4
+bulletY_change = 10
+bullet_state = "ready"
 
 
 def player(x,y):
@@ -33,12 +47,19 @@ def player(x,y):
 def enemy(x,y):
     screen.blit(enemyImg,(x,y)) #blit means to draw
 
+def fire_bullet(x,y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg,(x+16,y+10)) #to set the bullet 
+
 #Game Loop
 running = True
 while running:
      #rgb - red, green, blue (0 to 255)
     screen.fill((0,0,0))
-    
+    #background image
+    screen.blit(bg,(0,0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             running = False 
@@ -47,11 +68,15 @@ while running:
         if event.type == pygame.KEYDOWN:
             print("KEYSTROKE PRESSED(anykey)")
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.3
+                playerX_change = -5
                 # print("LEFT ARROW")
             if event.key == pygame.K_RIGHT:
                 # print("RIGHT ARROW")
-                playerX_change = 0.3
+                playerX_change = 5
+            if event.key == pygame.K_SPACE:
+                fire_bullet(playerX,bulletY)
+
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -63,6 +88,17 @@ while running:
         playerX = 0 
     elif playerX >= 736: #800-64 as 64 is the size of spaceship
         playerX = 736
+
+#movement of enemy
+    enemyX += enemyX_change
+
+    if enemyX<=0:
+        enemyX_change = 4
+        enemyY +=  enemyY_change 
+    elif enemyX >= 736: #800-64 as 64 is the size of spaceship
+        enemyX_change = -4 
+        enemyY +=  enemyY_change 
+
 
 
 
