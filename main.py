fc640cb46a3ad749f316c7ea5257737bc6074c00
Yashi Gupta <yashig406@gmail.coM>
2023-03-30 13:17:23 +0530

    adding the game over text

--- main.py
+++ main.py
@@ -62,9 +62,16 @@ font = pygame.font.Font('freesansbold.ttf',32)
 text_x = 10
 text_y =10
 
+#game over text 
+over_font = pygame.font.Font('freesansbold.ttf',64)
+
 def show_score(x,y):
     score = font.render("Score: " + str(score_value),True,(255,255,255))
     screen.blit(score,(x,y))
+
+def game_over_text():
+    over_text = over_font.render("GAME OVER",True,(255,255,255))
+    screen.blit(over_text,(200,250))
 
 def player(x,y):
     screen.blit(playerImg,(x,y)) #blit means to draw
@@ -128,6 +135,14 @@ while running:
 
 #movement of enemy
     for i in range(num_of_enemeies):
+
+        #GAME OVER
+        if enemyY[i] > 440:
+            for j in range(num_of_enemeies):
+                enemyY[j] = 2000
+            game_over_text()
+            break
+
         enemyX[i] += enemyX_change[i]
         if enemyX[i] <= 0:
             enemyX_change[i] = 4
