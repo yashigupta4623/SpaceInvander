991df3f2c457f152c847ca9a5288810656c0bc5f
Yashi Gupta <yashig406@gmail.coM>
2023-03-30 11:43:33 +0530

    adding the score system

--- main.py
+++ main.py
@@ -49,7 +49,16 @@ bulletX_change = 4
 bulletY_change = 6
 bullet_state = "ready"
 
-score = 0
+#score
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf',32)
+
+text_x = 10
+text_y =10
+
+def show_score(x,y):
+    score = font.render("Score: " + str(score_value),True,(255,255,255))
+    screen.blit(score,(x,y))
 
 def player(x,y):
     screen.blit(playerImg,(x,y)) #blit means to draw
@@ -124,8 +133,8 @@ while running:
         if collision:
             bulletY = 480 
             bullet_state = "ready"
-            score += 1 
-            print(score)
+            score_value += 1 
+            print(score_value)
             enemyX[i]  = random.randint(0,735)
             enemyY[i]  = random.randint(50,150)
 
@@ -143,5 +152,5 @@ while running:
  
 
     player(playerX,playerY)
-   
+    show_score(text_x,text_y)
     pygame.display.update() #MANDATORY
